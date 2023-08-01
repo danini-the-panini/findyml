3ae65f170a403e261eda16535f96b1c4a132c43c
Dani Smith <code@danini.dev>
2023-08-02 01:02:43 +0200

    better key finding logic

--- lib/findyml.rb
+++ lib/findyml.rb
@@ -7,29 +7,39 @@ require_relative "findyml/version"
 module Findyml
   class Error < StandardError; end
 
-  def self.find(key, dir: Dir.pwd)
+  def self.find(key, dir = Dir.pwd)
     # TODO: cache fast key lookup in a temporary sqlite db?
     key_path = key.split('.').map { |k|
       k.match?(/\d+/) ? k.to_i : k
     }
     files = Dir.glob(File.join(dir, '**', '*.yml'))
     files.each do |file|
-      # TODO: do our own parsing or otherwise find a way to get line numbers
-      #       we also want to be super permissive and fast
-      #       we don't care about the values
-      #       and we want to keep the keys as strings
-      #       oh and we want to have line numbers!
+      # TODO: use YAML.parse to get raw nodes with line numbers
+      #       or build our own custom handler
       yaml = YAML.unsafe_load_file(file)
 
-      # TODO: if a value is _actually_ nil then this will give a false negative
-      #       custom parser could do a line-by-line stream rather
-      #       and just care about key presence
-      #       also this raises if you try dig into a terminal value, e.g. string
-      puts file if yaml.dig(*key_path)
-    rescue YAML::SyntaxError => e
+      puts file if deep_key_presence?(yaml, key_path)
+    rescue YAML::SyntaxError
       # just skip files we can't parse
       # TODO: silence warnings?
       warn "Skipping #{file} due to parse error"
+    end
+  end
+
+  def self.deep_key_presence?(obj, key_path)
+    return true if key_path.empty?
+    key, *rest = key_path
+
+    case obj
+    when Hash then
+      return false unless obj.key?(key)
+      deep_key_presence?(obj[key], rest)
+    when Array then
+      return false unless key.is_a?(Integer)
+      return false if key >= obj.size || key < 0
+      deep_key_presence?(obj[key], rest)
+    else
+      false
     end
   end
 end
--- test/test_findyml.rb
+++ test/test_findyml.rb
@@ -7,7 +7,33 @@ class TestFindyml < Minitest::Test
     refute_nil ::Findyml::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_deep_key_presence?
+    assert Findyml.deep_key_presence?({ 'foo' => 'bar' }, ['foo'])
+    assert Findyml.deep_key_presence?({ 'foo' => { 'bar' => 'baz' } }, ['foo', 'bar'])
+    assert Findyml.deep_key_presence?({ 'foo' => { 'bar' => 'baz' } }, ['foo'])
+    assert Findyml.deep_key_presence?({ 'foo' => ['bar', 'baz'] }, ['foo', 0])
+    assert Findyml.deep_key_presence?({ 'foo' => ['bar', 'baz'] }, ['foo', 1])
+    assert Findyml.deep_key_presence?({ 'foo' => [{ 'bar' => 'baz' }, { 'qux' => 'norf' }] }, ['foo', 1, 'qux'])
+
+    refute Findyml.deep_key_presence?({ 'foo' => 'bar' }, ['bar'])
+    refute Findyml.deep_key_presence?({ 'foo' => 'bar' }, ['foo', 'bar'])
+    refute Findyml.deep_key_presence?({ 'foo' => { 'bar' => 'baz' } }, ['foo', 'baz'])
+    refute Findyml.deep_key_presence?({ 'foo' => { 'bar' => 'baz' } }, ['bar', 'baz'])
+    refute Findyml.deep_key_presence?({ 'foo' => { 'bar' => 'baz' } }, ['foo', 'bar', 'baz'])
+    refute Findyml.deep_key_presence?({ 'foo' => ['bar', 'baz'] }, ['foo', 2])
+    refute Findyml.deep_key_presence?({ 'foo' => ['bar', 'baz'] }, ['foo', 'bar'])
+    refute Findyml.deep_key_presence?({ 'foo' => ['bar', 'baz'] }, ['foo', -1])
+    refute Findyml.deep_key_presence?({ 'foo' => [{ 'bar' => 'baz' }, { 'qux' => 'norf' }] }, ['foo', 1, 'bar'])
+    refute Findyml.deep_key_presence?({ 'foo' => [{ 'bar' => 'baz' }, { 'qux' => 'norf' }] }, ['foo', 2, 'bar'])
+  end
+
+  def test_find
+    out, err = capture_io do
+      Findyml.find('foo.bar', File.join(__dir__, 'yaml'))
+    end
+
+    assert_equal File.join(__dir__, 'yaml', 'another_example.yml').to_s, out.lines[0].chomp
+    assert_equal File.join(__dir__, 'yaml', 'example.yml').to_s, out.lines[1].chomp
+    assert_empty err
   end
 end
